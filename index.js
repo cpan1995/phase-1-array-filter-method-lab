f8db0e8c49fc00746d1daa03d8464855caebb3da
cpan1995 <cpan1995@gmail.com>
2021-06-28 22:20:04 -0400

    commit tree 33098e328e52cb54d70ddafd749805100800a1e6

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(driversArray, nameOfDriver){
+    let result = driversArray.filter(element => nameOfDriver.toLowerCase() == element.toLowerCase());
+    return result;
+}
+
+function fuzzyMatch(driversArray, someString){
+    let result = driversArray.filter(element => element.startsWith(someString));
+    return result;
+}
+
+function matchName(driversObjectArray, someString){
+    let result =  driversObjectArray.filter(element => element.name === someString);
+    return result;
+}
